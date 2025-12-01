fd1672b84f3e031d77ec85e5bc852f0557fe3679
anaalonso8 <ana.alonsomasso@edu.uah.es>
2025-12-01 21:27:46 +0100

    Update contador.py

--- src/contador.py
+++ src/contador.py
@@ -1,1 +1,17 @@
+mkdir -p src
+nano src/contador.py   
+
+import time
+import sys
+
+def main():
+    for i in range(1, 11):
+        print(i, flush=True)
+        time.sleep(1)
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Interrumpido", file=sys.stderr)
 
